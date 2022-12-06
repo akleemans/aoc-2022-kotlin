0d6a5ff9cac7404764933320d6b5d79ce0ee2a25
Adrianus Kleemans <a.kleemans@gmail.com>
2022-12-06 13:42:33 +0100

    Add Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,41 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var highest = 0
+        var currentCounter = 0
+        for (line in input) {
+            if (line.isNotEmpty()) {
+                currentCounter += line.toInt()
+            } else {
+                if (highest < currentCounter) {
+                    highest = currentCounter
+                }
+                currentCounter = 0
+            }
+        }
+        return highest
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val numberList = ArrayList<Int>()
+        var currentCounter = 0
+        for (line in input) {
+            if (line.isNotEmpty()) {
+                currentCounter += line.toInt()
+            } else {
+                numberList.add(currentCounter)
+                currentCounter = 0
+            }
+        }
+        numberList.add(currentCounter)
+
+        return numberList.sortedDescending().take(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test_input")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("Day01_input")
+    println("Part 1: " + part1(input))
+    println("Part 2: " + part2(input))
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -2,11 +2,13 @@ import java.io.File
 import java.math.BigInteger
 import java.security.MessageDigest
 
-/**
- * Reads lines from the given input txt file.
- */
 fun readInput(name: String) = File("src", "$name.txt")
     .readLines()
+
+/*
+fun readIntInput(name: String) = File("src", "$name.txt")
+    .readLines().stream().map(v -> v.toInt())
+*/
 
 /**
  * Converts string to md5 hash.
